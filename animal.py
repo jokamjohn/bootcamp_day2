1041f5d474ef91709812066032a443a1bb2376b3
John Kagga <johnkagga@gmail.com>
2017-07-04 12:19:02 +0300

    Added base animal class from which all animals will be based on

---
+++ animal.py
@@ -0,0 +1,19 @@
+class Animal:
+    """
+    A base animal class
+    """
+    animal_type = ''
+    num_of_legs = 4
+    sound = 'roar'
+    num_of_horns = 0
+
+    def make_sound(self):
+        """Return the sound the animal makes three times"""
+        return self.sound.upper() * 2
+
+    def has_horns(self):
+        """Returns boolean if the animal has horns"""
+        if self.num_of_horns > 0:
+            return True
+        else:
+            return False
